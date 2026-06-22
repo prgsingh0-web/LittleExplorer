a392c36bb8bb17f1a50c73e5d9bb8acd09a563b4
Pragya Singh <pragyasingh@Pragyas-MacBook-Air.local>
2026-06-22 23:21:22 +0400

    Add authentication and login

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,9 +1,11 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using ToddlerActivityPlanner.Models;
 
+
 namespace ToddlerActivityPlanner.Data;
 
-public class AppDbContext : DbContext
+public class AppDbContext : IdentityDbContext<ApplicationUser>
 {
     public AppDbContext(DbContextOptions<AppDbContext> options)
         : base(options)
--- Program.cs
+++ Program.cs
@@ -1,7 +1,9 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 using ToddlerActivityPlanner.Components;
 using ToddlerActivityPlanner.Data;
 using ToddlerActivityPlanner.Services;
+using ToddlerActivityPlanner.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +12,16 @@ builder.Services.AddRazorComponents()
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite("Data Source=activities.db"));
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
+{
+    options.SignIn.RequireConfirmedAccount = false;
+})
+.AddEntityFrameworkStores<AppDbContext>()
+.AddDefaultTokenProviders();
+
+builder.Services.AddRazorPages(); 
+
 
 builder.Services.AddScoped<ActivityService>();
 builder.Services.AddScoped<MealService>();
@@ -30,6 +42,10 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseStaticFiles();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
 app.UseAntiforgery();
 
 app.MapRazorComponents<App>()
@@ -38,7 +54,31 @@ app.MapRazorComponents<App>()
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    db.Database.EnsureCreated();
+    db.Database.Migrate();
 }
+app.MapRazorPages();
+
+app.MapPost("/login-user", async (
+    HttpContext httpContext,
+    SignInManager<ApplicationUser> signInManager) =>
+{
+    var form = await httpContext.Request.ReadFormAsync();
+
+    var email = form["email"].ToString();
+    var password = form["password"].ToString();
+
+    var result = await signInManager.PasswordSignInAsync(
+        email,
+        password,
+        isPersistent: true,
+        lockoutOnFailure: false);
+
+    if (result.Succeeded)
+    {
+        return Results.Redirect("/dashboard");
+    }
+
+    return Results.Redirect("/login?error=1");
+});
 
 app.Run();
